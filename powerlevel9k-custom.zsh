a663523b3d2522f82e1149b0242fae5859917b47
ryandroll <ryandroll@gmail.com>
2018-03-30 02:12:24 +0800

    debug SECOND_PROMPT_PREFIX

--- powerlevel9k-custom.zsh
+++ powerlevel9k-custom.zsh
@@ -15,4 +15,10 @@ POWERLEVEL9K_STATUS_VERBOSE=false
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status anaconda vi_mode)
 POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="\n"
-POWERLEVEL9K_MULTILINE_SECOND_PROMPT_PREFIX="%K{white}%F{black} %h %f%k%F{white}%f "
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%K{white}%F{black} %h %f%k%F{white}%f "
+
+## Reference
+## https://github.com/bhilburn/powerlevel9k/wiki/Show-Off-Your-Config
+## The best
+## Semartin's Configuration:
+## POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%K{white}%F{black} \UE12E `date +%T` %f%k%F{white}%f "
