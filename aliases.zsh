f3a2879ad0ff46d64361d311ec3a1575714f1e1d
Ryandr0ll <19866320+ryandroll@users.noreply.github.com>
2024-07-10 02:44:41 +0800

    Update aliases.zsh
    add upgrade section

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,7 @@
-# conda and miniconda
+# Upgrade
+alias upgrade_zinit='zinit self-update && zinit update -a -p && zinit compinit'
+
+# Mamba and Micromamba
 if (( $+commands[micromamba] )); then
     alias mamba=micromamba
     alias conda=micromamba
