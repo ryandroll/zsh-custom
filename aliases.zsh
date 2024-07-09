669e5cd8b8cc701f7e145417a47da2385cfcf41b
Ryandr0ll <19866320+ryandroll@users.noreply.github.com>
2024-07-10 03:57:13 +0800

    Update aliases.zsh
    Add OS bundles

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,20 @@
+# OS bundles
+if [[ $OSTYPE == darwin* ]]; then
+    if (( $+commands[brew] )); then
+        alias bu='brew update; brew upgrade; brew cleanup'
+        alias bcu='brew cu --all --yes --cleanup'
+        alias bua='bu; bcu'
+    fi
+elif [[ $OSTYPE == linux* ]]; then
+    if (( $+commands[apt-get] )); then
+        alias agua='aguu -y && agar -y && aga -y'
+        alias kclean+='sudo aptitude remove -P "?and(~i~nlinux-(ima|hea),\
+                            ?not(?or(~n`uname -r | cut -d'\''-'\'' -f-2`,\
+                            ~nlinux-generic,\
+                            ~n(linux-(virtual|headers-virtual|headers-generic|image-virtual|image-generic|image-`dpkg --print-architecture`)))))"'
+    fi
+fi
+
 # Upgrade
 alias upgrade_zinit='zinit self-update && zinit update -a -p && zinit compinit'
 
