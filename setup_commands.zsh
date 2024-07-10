4191c305aebb52c0e6333f78a25fa60f79f8ebae
Ryandr0ll <19866320+ryandroll@users.noreply.github.com>
2024-07-11 00:46:46 +0800

    Update setup_commands.zsh
    remove mamba settings just using micromamba

--- setup_commands.zsh
+++ setup_commands.zsh
@@ -47,25 +47,6 @@ if (( $+commands[fzf] )); then
     export FZF_ALT_C_OPTS="--preview '(eza --tree --icons --level 3 --color=always --group-directories-first {} || tree -NC {} || ls --color=always --group-directories-first {}) 2>/dev/null | head -200'"
 fi
 
-# Mambaforge
-if [[ -f "/opt/mambaforge/bin/conda" ]] ; then
-      __conda_setup="$('/opt/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-      if [ $? -eq 0 ]; then
-      eval "$__conda_setup"
-      else
-      if [ -f "/opt/mambaforge/etc/profile.d/conda.sh" ]; then
-            . "/opt/mambaforge/etc/profile.d/conda.sh"
-      else
-            export PATH="/opt/mambaforge/bin:$PATH"
-      fi
-      fi
-      unset __conda_setup
-
-      if [ -f "/opt/mambaforge/etc/profile.d/mamba.sh" ]; then
-      . "/opt/mambaforge/etc/profile.d/mamba.sh"
-      fi
-fi
-
 # Micromamba 
 if (( $+commands[micromamba] )); then
     # Set environment variables for micromamba
@@ -73,7 +54,7 @@ if (( $+commands[micromamba] )); then
     export MAMBA_ROOT_PREFIX='/opt/micromamba'
 
     # Setup micromamba shell integration
-    __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+    __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
     if [ $? -eq 0 ]; then
         eval "$__mamba_setup"
     else
